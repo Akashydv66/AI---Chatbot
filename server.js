26fc1d1c15c07073de6e3b0beb9243ca9f75c02b
Akash Yadav <ay293044@gmail.com>
2025-10-25 05:29:35 +0530

    Sync local files

--- server.js
+++ server.js
@@ -6,7 +6,28 @@ const { Server } = require("socket.io");
 const generateResponse = require('./src/services/ai.service')
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, {
+  cors:{
+    origin:"http://localhost:5173",
+  }
+ });
+
+const chatHistory = [
+  {
+    role:"user",
+     parts: [{ text: 'Who was the PM of India in 1984?' }]
+  },
+  {
+    role:"model",
+    parts:[
+      {
+         text:"Indira Gandhi was the Prime Minister of India in 1984. She served until her assassination on October 31, 1984. After her death, her son, Rajiv Gandhi, became the Prime Minister."
+      },
+    ],
+  }
+]
+
+
 io.on("connection", (socket) => {
   console.log("A user connected")
 
@@ -17,7 +38,17 @@ socket.on("disconnect", (socket) => {
 socket.on("ai-message", async(data)=>{
      
      console.log("Received AI message:", data.prompt); 
-    const response = await generateResponse(data.prompt);
+     chatHistory.push({
+      role:"user",
+      parts:[{text: data.prompt}]
+     })
+    const response = await generateResponse(chatHistory);
+
+    chatHistory.push({
+      role:"model",
+      parts:[{text:response}]
+    })
+
     console.log("Ai Response", response);
     socket.emit("ai-message-response", {response})
 })
